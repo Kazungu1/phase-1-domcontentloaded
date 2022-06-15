38fbd8076ab969443bfe954bd96c1ef5659de8e2
Kazungu1 <kazunguderriq@gmail.com>
2022-06-15 15:53:54 +0300

    commit tree 155741e396678021de96a51cf90272cdd06d9a7d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById( 'text' )
+    .textContent ="This is really cool!" 
+    })
+
+    // document.addEventListener("DOMContentLoaded", ()=> {
+    
+    //     document.getElementById("text").innerHTML = "This is really cool!";
+        
+    //   });
+      
